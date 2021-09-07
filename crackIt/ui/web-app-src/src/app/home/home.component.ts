290cfac853bc68569fd11481f1a419f2f3ff5b27
AruWipro <piratlaaru@gmail.com>
2021-09-07 16:57:23 +0530

    Update home.component.ts

--- crackIt/ui/web-app-src/src/app/home/home.component.ts
+++ crackIt/ui/web-app-src/src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {NgsRevealConfig} from 'ngx-scrollreveal'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [NgsRevealConfig]
 })
 export class HomeComponent implements OnInit {
 
   imageObject: any;
 
-  constructor() {
-
+  constructor(config: NgsRevealConfig) {
+    config.duration = 5000;
+    config.easing = 'cubic-bezier(0.645, 0.045, 0.355, 1)';
+    config.origin = 'left'
   }
 
   ngOnInit() {
